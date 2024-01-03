f92d67b5182457880ad8e3af255521addda7eb60
poko <minshanzen@gmail.com>
2024-01-03 11:41:02 +0800

    [add] 複製分享連結功能、使用說明

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+if (getParameterByName('code') != null) {
+    document.querySelector('#input').value = getParameterByName('code');
+    checkDevice()
+} else {
+    document.querySelector('#input').removeAttribute('disabled');
+    document.querySelector('.go_btn').textContent = '點我歸戶';
+}
+
 function getParameterByName(name, url = window.location.href) {
     name = name.replace(/[\[\]]/g, '\\$&');
     const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
@@ -7,34 +15,72 @@ function getParameterByName(name, url = window.location.href) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-if (getParameterByName('code') != null) {
-    document.querySelector('#input').value = getParameterByName('code');
-    window.location.replace(
-        `foodpanda://?c=TW&s=o&code=${getParameterByName('code')}`,
-    );
-} else {
-    document.querySelector('#input').removeAttribute('disabled');
-}
-
-
-
 function toggleByBtn() {
     if (getParameterByName('code') === null) {
-        if (document.querySelector('#input').value.trim() === '') {
+        let code = document.querySelector('#input').value.trim();
+        if (code === '') {
             alert('請輸入需歸戶的序號');
             return
         }
-        let code = document.querySelector('#input').value.trim();
-        window.location.replace(
-            `foodpanda://?c=TW&s=o&code=${code}`,
-        );
-        return;
+        if (checkDevice(true)) {
+            window.location.replace(
+                `foodpanda://?c=TW&s=o&code=${code}`,
+            );
+        } else {
+            window.location.replace(
+                `https://www.foodpanda.com.tw/vouchers?code=${code}`,
+            );
+        }
     } else {
-        window.location.replace(
-            `foodpanda://?c=TW&s=o&code=${getParameterByName('code')}`,
-        );
+        checkDevice()
     }
+}
 
+function redirectByDesktop() {
+    window.location.replace(
+        `https://www.foodpanda.com.tw/vouchers?code=${getParameterByName('code')}`,
+    );
+}
+
+function redirectByMobile() {
+    window.location.replace(
+        `foodpanda://?c=TW&s=o&code=${getParameterByName('code')}`,
+    );
+}
+
+function checkDevice(onlyCallback = false) {
+    try {
+        document.createEvent("TouchEvent");
+        if (onlyCallback) {
+            return true
+        } else {
+            redirectByMobile()
+        }
+    } catch (e) {
+        if (onlyCallback) {
+            return false
+        } else {
+            redirectByDesktop()
+        }
+    }
 }
 
 
+function getShareLink(){
+    let shareLink = "https://pokotw.dev/panda";
+    let code = document.querySelector('#input').value.trim();
+    if (code === ''){
+        shareLink += `?code=改為你要分享的序號`;
+    }else{
+        shareLink += `?code=${code}`;
+    }
+    let input = document.createElement('input');
+    input.setAttribute('readonly', 'readonly');
+    input.setAttribute('value', shareLink);
+    document.body.appendChild(input);
+    input.select();
+    input.setSelectionRange(0, 9999);
+    document.execCommand('copy');
+    document.body.removeChild(input);
+    alert('已複製分享連結');
+}
